dd40f7926bb2223746cb6e05773c90854aa02240
Lindalvo Neto <netobrpa@gmail.com>
2024-12-07 17:25:48 -0300

    pequenos ajustes na questao 10

--- lista3/questao10.py
+++ lista3/questao10.py
@@ -2,10 +2,13 @@ def josephus(n, k):
     if n == 1: # caso base. resta uma pessoa no cículo
         return 0
     else: # Chama recursivo
-        return (josephus(n - 1, k) + k) % n
+        resultado = josephus(n - 1, k)
+        # k = deslocamento
+        # resto da divisão pela quantidade de pessoas no círculo
+        return (resultado + k) % n
 
 print("==== Problema da Lenda e Flavious Josephus ==== ")
-NC = int(input("Entre com um número entre 1 e 30 de casos para testar: "))
+NC = int(input("Entre com um número de casos entre 1 e 30 para testar: "))
 print("n = quantidade de pessoas no cículo")
 print("k = quantidade de passos entre duas pessoas no círculo")
 for Z in range(NC):
